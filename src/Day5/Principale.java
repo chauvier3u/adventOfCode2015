fe5364b9a464194108df0155ac8ca46bf731b5cd
chauvier3u <benoitchauviere@hotmail.fr>
2018-12-02 23:04:28 +0100

    day5 challenge 2

--- src/Day5/Principale.java
+++ src/Day5/Principale.java
@@ -9,6 +9,33 @@ public class Principale
 
 	public static void main(String[] args) throws FileNotFoundException
 	{
+		partOne();
+		partTwo();
+	}
+
+	public static void partTwo() throws FileNotFoundException
+	{
+		File fichier = new File("./src/Day5/data.txt");
+		Scanner sc = new Scanner(fichier);
+
+		String line = "";
+
+		int LettreNice = 0;
+
+		while (sc.hasNextLine())
+		{
+			line = sc.nextLine();
+			if (partTwoUn(line) && partTwoDeux(line))
+			{
+				LettreNice++;
+			}
+		}
+		System.out.println(LettreNice);
+		sc.close();
+	}
+
+	public static void partOne() throws FileNotFoundException
+	{
 		File fichier = new File("./src/Day5/data.txt");
 		Scanner sc = new Scanner(fichier);
 
@@ -72,5 +99,32 @@ public class Principale
 			}
 		}
 		return true;
+	}
+
+	public static boolean partTwoUn(String input)
+	{
+		for (int i = 0; i < input.length() - 2; i++)
+		{
+			char a = input.charAt(i);
+			char b = input.charAt(i + 1);
+			for (int j = i + 2; j < input.length() - 1; j++)
+			{
+				char c = input.charAt(j);
+				char d = input.charAt(j + 1);
+				if (a == c && b == d)
+					return true;
+			}
+		}
+		return false;
+	}
+
+	public static boolean partTwoDeux(String input)
+	{
+		for (int i = 0; i < input.length() - 2; i++)
+		{
+			if (input.charAt(i) == input.charAt(i + 2))
+				return true;
+		}
+		return false;
 	}
 }
